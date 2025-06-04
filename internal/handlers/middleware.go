c2819da7f9b1f94fa896d70f5852e5eb80ea5d14
rk5634 <rajiv0099c@gmail.com>
2025-06-04 15:12:25 +0530

    made some changes for CORS

--- internal/handlers/middleware.go
+++ internal/handlers/middleware.go
@@ -121,3 +121,23 @@ func AdvancedRateLimitMiddleware(redisClient *redis.Client, maxTokens int, refil
 		c.Next()
 	}
 }
+
+
+
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		// Handle preflight OPTIONS request
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
